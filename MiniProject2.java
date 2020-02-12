38ed503e922ea949d01bf88443b5f40bbf9a0a1a
ryanpinkney <60120397+ryanpinkney@users.noreply.github.com>
2020-02-12 20:11:34 +0000

    Add files via upload
    i worked on the mini-project two and i think it fulfills all of the requirements.

--- MiniProject2.java
+++ MiniProject2.java
@@ -2,20 +2,33 @@ import java.util.*;
 public class MiniProject2 {
 
 	public static synchronized void main(String[] args) throws InterruptedException {
-		//this is for determining the total number of slave threads
 		Scanner input = new Scanner(System.in);
+		
+		//this is for determining the total number of slave threads
 		System.out.println("how many slave threads do you want? ");
 		int N = input.nextInt();
+		
+		//this is for determing the maximum length of a request
 		System.out.println("how long should maximum request length be? ");
 		int M = input.nextInt();
 		
 		//this creates the master thread which queues requests
-		MasterThread Master = new MasterThread(N,M);
-		Slave[] slaves = new Slave[N];
-		for(int i = 0; i < N; i++) {
-			slaves[i] = new Slave(i, Master);
-			slaves[i].run();
+		long start = System.currentTimeMillis();
+		MasterThread master = new MasterThread(N,M, start);
+		
+		while(true) {
+			//prepares a new request
+			double requestTime = Math.random() * M;
+			master.Add((int) requestTime);
+			//lets user know whats happening
+			//tries to sleep a while before producing a new request
+			double sleepTime =(1000 * Math.random() * M);
+			try {
+				System.out.println("Producer: sleeping for " + sleepTime/1000 + " seconds");
+				Thread.sleep((int) sleepTime);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
-		Master.run();
 	}
 }
--- Slave.java
+++ Slave.java
@@ -1,19 +1,41 @@
 
 public class Slave extends Thread{
-	private MasterThread Master;
-	int slaveID;
+	//important data
+	private MasterThread master;
+	private int slaveID;
+	private long start;
 
-	public Slave(int id, MasterThread mas){
+	//simple constructor
+	public Slave(int id, MasterThread mas, long s){
 		slaveID = id;
-		Master = mas;
+		master = mas;
+		start = s;
 	}
-	public void run(){
-		Object command;
+	//primary part of the slave thread
+	public synchronized void run(){
+		//lets user know the slave has been created
+		System.out.println("slave " + slaveID + " has started");
+		while(true) {
+		//prepares dummy command variables incase of issues on try-catch
+		int[] command = new int[2];
+		command[0] = 1;
+		command[1] = 1;
+		//attempts to call the master-threads queue for a request
 		try {
-			command = Master.Remove();
+			command = (int[]) master.Remove();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		System..out.println("Consumer " + slaveID +": " );
+		//informs user that a request went through
+		System.out.println("Consumer " + slaveID +": assigned request ID " + command[1] + "processing request for the next " + command[0] +" seconds, " + "current time is " + start);
+		//slave processes request, represented by going to sleep
+		try {
+			Thread.sleep(command[0]);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		//inform user that it completed request
+		System.out.println("Consumer " + slaveID + ": completed request ID " + command[1] + " at time " + System.currentTimeMillis());
+		}
 	}
 }
