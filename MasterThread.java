2c69e1371ab80aa92b2dcd38acf4b6fb8444f167
ryanpinkney <60120397+ryanpinkney@users.noreply.github.com>
2020-02-12 20:38:30 +0000

    Add files via upload
    Cleaned up some lines that are no longer required.

--- MasterThread.java
+++ MasterThread.java
@@ -1,43 +1,38 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class  MasterThread {
-	private int totalslaves;
-	private int length;
+public class  MasterThread extends Thread {
+	//various private variables for the thread
 	private Queue request = new LinkedList();
-	private int ID = 0;
+	private int iD = 0;
 	private long start;
+	private Slave[] slaves;
 	
-	MasterThread(int n, int m) throws InterruptedException{
-		totalslaves = n;
-		length = m;
-		start = System.currentTimeMillis();
+	//constructor prepares all the necessary slave threads
+	public MasterThread(int n, int m, long s) throws InterruptedException{
+		start = s;
+		slaves = new Slave[n];
+		for(int i = 0; i < n; i++) {
+			slaves[i] = new Slave(i, this, start);
+			slaves[i].start();
+		}
 	}
-	public synchronized void Add(Object item, int ID) {	
-		request.add(item);
-		ID++;
+	//the add function queues a new request and attempts to notify a slave
+	public synchronized void Add(int item) {	
+		int req[] = new int[2];
+		req[0] = item;
+		req[1] = iD;
+		request.add(req);
+		System.out.println("Producer: produced request ID " + iD + ", length " + item + " at time " + System.currentTimeMillis());
+		iD++;
 		notify();
-	}
+		}
+	//this method takes the head of the queue and returns it
 	public synchronized Object Remove() throws InterruptedException {
-		while(request.peek() != null) {
-			wait();
+		while(request.peek() == null) {
+				wait();
 		}
 		Object returnable = request.remove();
 		return returnable;
 	}
-	public synchronized void run() {
-		while(true) {
-			int requestTime = (int) Math.random() * 10;
-			Add(requestTime, ID);
-			System.out.println("Producer: produced request ID " + ID + ", length " + (requestTime)/1000 + " at time " + (start - System.currentTimeMillis())/1000);
-			int sleepTime = (int) (1000 + Math.random() * length);
-			try {
-				Thread.sleep(sleepTime);
-				System.out.println("Producer: sleeping for " + sleepTime + " seconds");
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
 }
